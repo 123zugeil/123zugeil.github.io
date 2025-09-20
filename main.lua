c21e58d2dccc630d7c7aa329848078604f3e0b17
123zugeil <130390492+123zugeil@users.noreply.github.com>
2025-09-20 17:44:58 +0200

    Added Faster Scroll

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 local DuiObject = MachoCreateDui("https://123zugeil.github.io/index.html")
-MachoShowDui(DuiObject)
+local ShowMenu = false  -- Zeigt an, ob das Menü angezeigt wird
 
+-- Funktion zum Senden einer Nachricht an die DUI
 function moveToNextOption()
     local message = json.encode({
         type = 'moveDown'  -- Benutzerdefinierter Nachrichtentyp
@@ -8,11 +9,28 @@ function moveToNextOption()
     MachoSendDuiMessage(DuiObject, message)  -- Verwendet DuiObject statt duiObject
 end
 
+-- Überprüfen, ob der Pfeil nach unten gedrückt wurde, um das Menü zu zeigen
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
-        if IsControlJustPressed(0, 173) then  -- Pfeil nach unten
-            moveToNextOption()  -- Nachricht senden
+
+        -- Wenn die Taste für das Öffnen des Menüs gedrückt wird
+        if IsControlJustPressed(0, 74) then  -- Pfeil nach unten (Taste 74)
+            ShowMenu = true
+            MachoShowDui(DuiObject)
+        end
+
+        -- Wenn das Menü angezeigt wird und die Pfeil-nach-unten-Taste gehalten wird
+        if ShowMenu and IsControlPressed(0, 173) then  -- Pfeil nach unten (Taste 173)
+            moveToNextOption()  -- Nachricht senden, um nach unten zu scrollen
+            Citizen.Wait(200)  -- Warten, um eine kontinuierliche Bewegung zu ermöglichen (alle 100ms)
+        end
+
+        -- Wenn die Taste zum Schließen des Menüs gedrückt wird
+        if IsControlJustPressed(0, 305) then  -- ESC-Taste oder eine andere Taste zum Schließen
+            MachoHideDui(DuiObject)
+            MachoDestroyDui(DuiObject)  -- Zerstört das DUI
+            ShowMenu = false  -- Menü ausblenden
         end
     end
 end)
